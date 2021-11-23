f3846246a91f7185c6f56808a4a88f1b594b8b11
Dirk Eddelbuettel <edd@debian.org>
2021-11-23 09:18:07 -0600

    actually test for error in TileDB (with PR addressing this)

--- inst/tinytest/test_misc.R
+++ inst/tinytest/test_misc.R
@@ -10,4 +10,5 @@ ctx <- tiledb_ctx(limitTileDBCores())
 if (tiledb_version(TRUE) < "2.5.0") exit_file("These tests require TileDB 2.5.0 or newer")
 
 if (is.null(get0("tiledb_error_message"))) exit_file("No 'tiledb_error_message'")
-expect_true(inherits(tiledb_error_message(), "character"))
+#expect_true(inherits(tiledb_error_message(), "character"))
+expect_error(tiledb_error_message())
